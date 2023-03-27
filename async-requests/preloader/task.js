3641f3edfca5f2173a2dfff778f6e12cf2b7e05a
Надя Десятова <nadindinina@yandex.ru>
2023-03-27 19:05:56 +0300

    Выполнена задача 1 Анимация загрузки данных

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -2,37 +2,24 @@ const xhr = new XMLHttpRequest();
 const animation = document.getElementById('loader');
 const items = document.getElementById('items');
 
-function addValute(item) {
-  let newItem = document.createElement('div');
-  newItem.className = 'item';
-  let newCode = document.createElement('div');
-  newCode.textContent = item['CharCode'];
-  newCode.className = 'item__code';
-  let newValue = document.createElement('div');
-  newValue.textContent = item['Value'];
-  newValue.className = 'item__value';
-  let newCurrency = document.createElement('div');
-  newCurrency.textContent = 'руб.';
-  newCurrency.className = 'item__currency';
-  items.appendChild(newItem);
-  newItem.appendChild(newCode);
-  newItem.appendChild(newValue);
-  newItem.appendChild(newCurrency);
-}
-
 xhr.addEventListener('readystatechange', () => {
   if (xhr.readyState === xhr.DONE) {
     animation.className = 'loader';
-  	let allInformation = xhr['response'];
-    let objectСurrencies = allInformation['Valute'];
+    let data = JSON.parse(xhr.responseText);
+  	let allResponse = data['response'];
+    let objectСurrencies = allResponse['Valute'];
     let arrayСurrencies = Object.values(objectСurrencies);
-    arrayСurrencies.forEach(item => addValute(item));
+    for (i = 0; i < arrayСurrencies.length; i++) {
+      items.insertAdjacentHTML('beforeEnd', '<div class="item"><div class="item__code"></div><div class="item__value"></div><div class="item__currency">руб.</div></div>');
+      document.querySelectorAll('.item__code')[i].textContent = arrayСurrencies[i]['CharCode'];
+      document.querySelectorAll('.item__value')[i].textContent = arrayСurrencies[i]['Value'];
+    }
   } else {
       animation.className = 'loader loader_active';
     }
 });
 
-xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses', true);
 
 xhr.send();
 
