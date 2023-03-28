448774e7c6ec1607f5605a838c48b1fd4e11e01a
Надя Десятова <nadindinina@yandex.ru>
2023-03-28 12:39:14 +0300

    Выполнена задача 2 Опрос с выбором результатов

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,6 +1,7 @@
 const xhr1 = new XMLHttpRequest();
 const pollTitle = document.getElementById('poll__title');
 const pollAnswers = document.getElementById('poll__answers');
+const poll = document.querySelector('.poll');
 
 xhr1.addEventListener('readystatechange', () => {
   if (xhr1.readyState === xhr1.DONE) {
@@ -9,7 +10,7 @@ xhr1.addEventListener('readystatechange', () => {
   	let data = allResponse['data'];
     pollTitle.textContent = data['title'];
     let arrayAnswers = data['answers'];
-    for (i = 0; i < arrayAnswers.length; i++) {
+    for (let i = 0; i < arrayAnswers.length; i++) {
       pollAnswers.insertAdjacentHTML('beforeEnd', '<button class="poll__answer"></button>');
       document.querySelectorAll('.poll__answer')[i].textContent = arrayAnswers[i];
       let argumentForSend = 'vote=' + pollId + '&answer='+ i;
@@ -22,7 +23,18 @@ xhr1.addEventListener('readystatechange', () => {
 
         xhr2.addEventListener('readystatechange', () => {
           if (xhr2.readyState === xhr2.DONE){
-
+            pollAnswers.remove();
+            let allResponse2 = JSON.parse(xhr2.responseText);
+            let statistics = allResponse2['stat'];
+            let votesNumber = +statistics.reduce((accum, item) => accum + item['votes'], 0);
+            for (let i = 0; i < statistics.length; i++) {
+              poll.insertAdjacentHTML('beforeEnd', '<div class="poll__title"></div>');
+              poll.querySelectorAll('.poll__title')[i + 1].textContent = statistics[i]['answer'];
+              let votesNumberOfAnswer = ' ' + ((statistics[i]['votes'] / votesNumber) * 100).toFixed(2) + '%';
+              let pollTitleCurrent = poll.querySelectorAll('.poll__title')[i + 1];
+              pollTitleCurrent.insertAdjacentHTML('beforeEnd', '<b></b>')
+              pollTitleCurrent.querySelector('b').textContent = votesNumberOfAnswer;
+            }
           }
         });
 
