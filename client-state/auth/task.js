b161291c82ede3a7ba760f91a23f071b4d4533e1
Надя Десятова <nadindinina@yandex.ru>
2023-03-29 18:01:49 +0300

    Выполнена задача 3 Авторизация

---
+++ client-state/auth/task.js
@@ -0,0 +1,39 @@
+const signin = document.getElementById('signin');
+const signinForm = document.getElementById('signin__form');
+const signinButton = document.getElementById('signin__btn');
+const welcome = document.getElementById('welcome');
+const userId = document.getElementById('user_id');
+const storedId = localStorage.getItem('storedUserId');
+if (storedId) {
+  signin.classList.remove('signin_active');
+  welcome.classList.add('welcome_active');
+  userId.textContent = storedId;
+}
+
+signinButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(signinForm);
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === xhr.DONE) {
+      let response = JSON.parse(xhr.responseText);
+      if (response['success'] === false) {
+        alert('Неверный логин/пароль');
+        signinForm.reset();
+      } else {
+        signin.classList.remove('signin_active');
+        welcome.classList.add('welcome_active');
+        let storedUserId = response['user_id'];
+        userId.textContent = storedUserId;
+        localStorage.storedUserId = storedUserId;  
+      }
+    }
+  });
+
+  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth', true);
+
+  xhr.send(formData);
+});
