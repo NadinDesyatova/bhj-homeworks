e681108dd3564133b506a31722f5208e7a7f8366
Надя Десятова <nadindinina@yandex.ru>
2023-03-30 11:39:39 +0300

    Задача 3 "Авторизация" скорректирована

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -17,23 +17,23 @@ signinButton.addEventListener('click', (e) => {
 
   const xhr = new XMLHttpRequest();
 
-  xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === xhr.DONE) {
-      let response = JSON.parse(xhr.responseText);
-      if (response['success'] === false) {
-        alert('Неверный логин/пароль');
-        signinForm.reset();
-      } else {
-        signin.classList.remove('signin_active');
-        welcome.classList.add('welcome_active');
-        let storedUserId = response['user_id'];
-        userId.textContent = storedUserId;
-        localStorage.storedUserId = storedUserId;  
-      }
+  xhr.addEventListener('load', () => {
+    let response = xhr.response;
+    if (response['success'] === false) {
+      alert('Неверный логин/пароль');
+      signinForm.reset();
+    } else {
+      signin.classList.remove('signin_active');
+      welcome.classList.add('welcome_active');
+      let storedUserId = response['user_id'];
+      userId.textContent = storedUserId;
+      localStorage.storedUserId = storedUserId;  
     }
   });
 
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth', true);
+
+  xhr.responseType = 'json';
 
   xhr.send(formData);
 });
